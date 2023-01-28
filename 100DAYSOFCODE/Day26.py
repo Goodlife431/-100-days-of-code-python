2095f206cd85f53070f96bdf9e7dacff385649f7
Goodlife431 <111048769+Goodlife431@users.noreply.github.com>
2023-01-28 23:00:31 +0100

    commit

--- 100DAYSOFCODE/Day26.py
+++ 100DAYSOFCODE/Day26.py
@@ -1,13 +1,33 @@
+from replit import audio
 import os, time
 
-if __name__ == '__main__':
-    # for i in range(1, 1000):
-    #     print(i)
-    # os.system("clear")
-    print('Welcome')
-    print('To')
-    print('Replit')
-    time.sleep(1)
-    os.system('clear')
 
-    username = input('Username->')
+def play():
+    source = audio.play_file('audio.wav')
+    source.paused = False
+    while True:
+        play_back = int(input('press 5 to go back to main menu:'))
+        if play_back == 5:
+            source.paused = True
+            return
+        else:
+            continue
+
+
+while True:
+    print("My Pod Music Player")
+    time.sleep(1)
+    os.system("clear")
+    print("""
+  Press 1 to Play
+  Press 2 to exit
+  """)
+    press = int(input())
+    os.system("clear")
+    if press == 1:
+        print("Playing some proper tunes")
+        play()
+    if press == 2:
+        break
+    else:
+        continue
