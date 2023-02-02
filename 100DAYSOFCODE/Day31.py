49fb3a91654a3780a41313bc89e11e01feb80ea8
Goodlife431 <111048769+Goodlife431@users.noreply.github.com>
2023-02-02 16:21:39 +0100

    commit

---
+++ 100DAYSOFCODE/Day31.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+    app = f'\033[033m 🎶 Music app🎶'
+    print(f'{app:^47}')
+    print()
+    second_line = f'\033[037m 🔥▶️  Radio Gaga\n \t\tQueen'
+    print(f'{second_line}')
+    print()
+    print()
+    prev = f'\033[037m PREV'
+    print(f'{prev}')
+    next1 = f'\033[032m NEXT'
+    print(f'{next1:^20}')
+    pause = f'\033[035m PAUSE'
+    print(f'{pause:^30}')
+
---
+++ 100DAYSOFCODE/day31_second_project.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    welcome = f'\033[037m WELCOME TO'
+    print(f'{welcome:^40}')
+    arm = f'\033[034m --    ARMBOOK    --'
+    print(f'{arm:^42}')
+    text = f'\033[033m \t\tDefinitely not th rip off of\n\t\t\t a certain other social\n\t\t\t\t networking site'
+    print(f'{text:>43}')
+    print()
+    print('\t\t\t\033[031m honest.')
+    print('\t\t\t\033[037m username:\n  \t\t\tPassword: ')
