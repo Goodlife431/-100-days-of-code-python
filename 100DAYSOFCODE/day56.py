a42c6223c2c7a27aa1a659e76a87065e4fc3af5d
Goodlife431 <111048769+Goodlife431@users.noreply.github.com>
2023-02-22 14:47:58 +0100

    commit

--- 100DAYSOFCODE/day56.py
+++ 100DAYSOFCODE/day56.py
@@ -1,1 +1,17 @@
+import csv
+import os
+
 if __name__ == '__main__':
+    with open("100MostStreamedSongs.csv") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            dir_list = os.listdir()
+            artist = row["Artist(s)"].title()
+            print(artist)
+            if artist not in dir_list:
+                os.mkdir(artist)
+            song = row["Song"]
+            print(song)
+            path = os.path.join(f"{artist}/", song)
+            f = open(path, "w")
+            f.close()
